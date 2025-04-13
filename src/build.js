389b01ff7484526c1aa0bbf94f6771be4c1e90bc
salvadorsru <salvadorsru@gmail.com>
2025-04-13 02:02:55 +0200

    fix(build): ensure sourcemaps are consistently disabled for all builds

--- src/build.js
+++ src/build.js
@@ -6,24 +6,46 @@ try {
         bundle: true,
         minify: true,
         outfile: 'dist/index.min.js',
+        sourcemap: false,
+    })
+
+    await esbuild.build({
+        entryPoints: ['src/component/emulate.js'],
+        bundle: true,
+        minify: true,
+        outfile: 'dist/emulate.min.js',
+        sourcemap: false,
     })
 
     await esbuild.build({
         entryPoints: ['src/component/style.css'],
         minify: true,
         outfile: 'dist/style.min.css',
+        sourcemap: false,
     })
+
     await esbuild.build({
         entryPoints: ['src/component/index.js'],
         bundle: true,
         minify: true,
         outfile: 'demo/public/js/index.min.js',
+        sourcemap: false,
+    })
+    
+    await esbuild.build({
+        entryPoints: ['src/component/emulate.js'],
+        bundle: true,
+        format: 'esm',
+        minify: true,
+        outfile: 'demo/public/js/emulate.min.mjs',
+        sourcemap: false,
     })
 
     await esbuild.build({
         entryPoints: ['src/component/style.css'],
         minify: true,
         outfile: 'demo/public/css/style.min.css',
+        sourcemap: false,
     })
 } catch {
     process.exit(1)
